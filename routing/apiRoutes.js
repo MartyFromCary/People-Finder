ea0413610191dac6bf66906aa966f5cad4b55d4e
Elias A Martinez <MartyFromCary@gmail.com>
2018-03-08 15:26:44 -0500

    Almost Done

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -5,8 +5,32 @@ module.exports = app => {
     res.json(friendList);
   });
 
-  app.post("/api/friends", (req, res) => {
-    console.log(req.body);
-    res.send(friendList[0]);
+  app.post("/api/friends", ({ body }, res) => {
+    body.scores = body.scores.map(S => parseInt(S));
+    if (friendList.length == 0) {
+      res.json({
+        name: "UNKNOWN",
+        photo:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpRlN16IYgpKdC-MfVUSDkXHz2H0wSs8z-Tp7-jckbMJxQlBN3"
+      });
+    }
+    var bestIndex = -1;
+    var bestScore = 100;
+    friendList.forEach((friend, friendIndex) => {
+      var localScore = friend.scores.reduce(
+        (sum, score, scoreIndex) =>
+          sum + Math.abs(score - body.scores[scoreIndex]),
+        0
+      );
+
+      if (localScore < bestScore) {
+        bestScore = localScore;
+        bestIndex = friendIndex;
+      }
+    });
+
+    friendList.push(body);
+    const { name, photo } = friendList[bestIndex];
+    res.send({ name, photo });
   });
 };
